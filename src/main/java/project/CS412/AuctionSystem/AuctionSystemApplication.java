3587457ffe318413131a5370c955082f8dccbf12
Nishanth Panthangi <nishanthkwel@gmail.com>
2023-04-29 15:37:16 -0500

    Initial commit

---
+++ src/main/java/project/CS412/AuctionSystem/AuctionSystemApplication.java
@@ -0,0 +1,13 @@
+package project.CS412.AuctionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSystemApplication.class, args);
+	}
+
+}
