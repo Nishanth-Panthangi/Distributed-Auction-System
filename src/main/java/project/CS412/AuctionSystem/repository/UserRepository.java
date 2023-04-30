0b95ac6bd44e89c1f430bd6c9035b2fd07f7b9c6
Nishanth Panthangi <nishanthkwel@gmail.com>
2023-04-29 20:51:07 -0500

    Fixed Import data JPA issue

--- src/main/java/project/CS412/AuctionSystem/repository/UserRepository.java
+++ src/main/java/project/CS412/AuctionSystem/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package project.CS412.AuctionSystem.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import project.CS412.AuctionSystem.model.User;
 
-public interface UserRepository extends JpaRepository<User,Long>{
+
+public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String username);
 }
